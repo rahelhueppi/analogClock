3f78ea68f65533439264b3135d521cbc3ab95532
rahelhueppi <r.hueppi2008@gmail.com>
2023-11-24 13:23:40 +0100

    Uhr funktioniert

--- analogClock.js
+++ analogClock.js
@@ -1,27 +1,29 @@
 function time() {
   let now = new Date();
   console.log(now);
-  let hour = 6;
-  let minute = 10;
-  let second = 50;
-  /*
-  let hour = now.getHours();
-  let minute = now.getMinutes();
-  let second = now.getSeconds();*/
+
+  let hour = 23;
+  let minute = 0;
+  let second = 1;
+
+  //   let hour = now.getHours();
+  //   let minute = now.getMinutes();
+  //   let second = now.getSeconds();
 
   //Stundenzeiger
   let hourHand = document.querySelector(".hour-hand");
-  var hourDegrees = ((hour % 12) / 12) * 360;
+  console.log(hour % 12);
+  var hourDegrees = ((hour % 12) / 12) * 360 + 90;
   hourHand.style.transform = "rotate(" + hourDegrees + "deg)";
 
   //Minutenzeiger
   let minuteHand = document.querySelector(".min-hand");
-  var minDegrees = (minute / 60) * 360;
+  var minDegrees = (minute / 60) * 360 + 90;
   minuteHand.style.transform = "rotate(" + minDegrees + "deg)";
 
   //Sekunderzeiger
   let secondHand = document.querySelector(".second-hand");
-  let secondDegrees = (second / 60) * 360;
+  let secondDegrees = (second / 60) * 360 + 90;
   secondHand.style.transform = "rotate(" + secondDegrees + "deg)";
 }
 
