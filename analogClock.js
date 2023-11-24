80ca16286112ade34fb26f1f19236c806fc56b6b
rahelhueppi <r.hueppi2008@gmail.com>
2023-11-24 11:26:22 +0100

    2

--- analogClock.js
+++ analogClock.js
@@ -1,27 +1,30 @@
 function time() {
   let now = new Date();
+  console.log(now);
+  let hour = 6;
+  let minute = 10;
+  let second = 50;
+  /*
   let hour = now.getHours();
   let minute = now.getMinutes();
-  let second = now.getSeconds();
+  let second = now.getSeconds();*/
 
   //Stundenzeiger
   let hourHand = document.querySelector(".hour-hand");
-  /*if (hour > 12) {
-    hour = hour - 12;
-  }*/
-  let hourDegree = (hour / 12) * 360;
-  hourHand.style.transform = "rotate(" + hourDegree + "deg)";
+  var hourDegrees = ((hour % 12) / 12) * 360;
+  hourHand.style.transform = "rotate(" + hourDegrees + "deg)";
 
   //Minutenzeiger
   let minuteHand = document.querySelector(".min-hand");
-  let minuteDegree = (minute / 60) * 360;
-  minuteHand.style.transform = "rotate(" + minuteDegree + "deg)";
+  var minDegrees = (minute / 60) * 360;
+  minuteHand.style.transform = "rotate(" + minDegrees + "deg)";
 
   //Sekunderzeiger
   let secondHand = document.querySelector(".second-hand");
-  let secondDegree = (second / 60) * 360;
-  secondHand.style.transform = "rotate(" + secondDegree + "deg)";
+  let secondDegrees = (second / 60) * 360;
+  secondHand.style.transform = "rotate(" + secondDegrees + "deg)";
 }
 
 // Aktualisieren der Uhr alle 1 Sekunde
-setInterval(time, 500);
+
+setInterval(time, 1000);
