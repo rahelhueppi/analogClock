c60cb01708ab21c2b9c06b0de11bf17156b28a28
rahelhueppi <r.hueppi2008@gmail.com>
2023-11-24 14:24:34 +0100

    fertig und kommentiert

--- analogClock.js
+++ analogClock.js
@@ -1,32 +1,25 @@
 function time() {
+  //define variables (now, hour, minute, second)
   let now = new Date();
-  console.log(now);
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
 
-  let hour = 23;
-  let minute = 0;
-  let second = 1;
+  //hour hand
+  let hourHand = document.querySelector(".hour-hand"); //this will search in the document ".hour-hand"
+  var hourDegrees = ((hour % 12) / 12) * 360 + 90; //it calculates the degrees of the clock. The modulo is needed, because a digitac clock has 24 hours and the analog just 12
+  hourHand.style.transform = "rotate(" + hourDegrees + "deg)"; //It is a CSS transformation that rotates the element by the previously calculated degrees.
 
-  //   let hour = now.getHours();
-  //   let minute = now.getMinutes();
-  //   let second = now.getSeconds();
-
-  //Stundenzeiger
-  let hourHand = document.querySelector(".hour-hand");
-  console.log(hour % 12);
-  var hourDegrees = ((hour % 12) / 12) * 360 + 90;
-  hourHand.style.transform = "rotate(" + hourDegrees + "deg)";
-
-  //Minutenzeiger
+  //minute hand
   let minuteHand = document.querySelector(".min-hand");
   var minDegrees = (minute / 60) * 360 + 90;
   minuteHand.style.transform = "rotate(" + minDegrees + "deg)";
 
-  //Sekunderzeiger
+  //second hand
   let secondHand = document.querySelector(".second-hand");
   let secondDegrees = (second / 60) * 360 + 90;
   secondHand.style.transform = "rotate(" + secondDegrees + "deg)";
 }
 
-// Aktualisieren der Uhr alle 1 Sekunde
-
+// update the time every second
 setInterval(time, 1000);
